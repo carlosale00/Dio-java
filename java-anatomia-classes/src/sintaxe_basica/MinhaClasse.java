d6adf0a135d6fcbc61d6bece5187bcac0dd2ef94
Carlos Alexandre <126966037+carlosale00@users.noreply.github.com>
2024-09-12 11:22:32 -0300

    arquivos com nomes alterados.

---
+++ java-anatomia-classes/src/sintaxe_basica/MinhaClasse.java
@@ -0,0 +1,12 @@
+package sintaxe_basica;
+public class MinhaClasse {
+  // corpo da classe
+
+
+  // método inicialização
+  // vai imprimir apenas uma mensagem no console.
+
+  public static void main(String [] args){
+    System.out.print("Olá, seja bemm vindo");
+  }
+}
