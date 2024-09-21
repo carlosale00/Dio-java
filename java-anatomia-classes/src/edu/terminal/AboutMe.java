96148279cb91999987c6f1f0835262e4d4a48cc6
Carlos Alexandre <126966037+carlosale00@users.noreply.github.com>
2024-09-20 22:42:59 -0300

    Programa com exemplo de iteração do usuário direto  no prompt de comando, já passando os parametros nele.

---
+++ java-anatomia-classes/src/edu/terminal/AboutMe.java
@@ -0,0 +1,21 @@
+package edu.terminal;
+
+/* 
+<h1> Iteração no terminal </h1>
+exemplo de programa que tem iteração do usuário 
+
+* no terminal deve se estar na pasta bin , onde contem o arquivo compilado 
+* usar o comando java AboutMe 1ºparam 2ºparam.
+* EX: java AboutMe Carlos Alexandre
+* que seram executados de forma dinamica no terminal.
+
+*/
+
+public class AboutMe {
+  public static void main(String[] args) {
+    String nome = args[0];
+    String sobrenome = args[1];
+    System.out.println("Meu nome é " + nome + " " + sobrenome );
+  }
+}
+  
