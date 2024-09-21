868087e097c11676fe0355fa019da85120b9420e
Carlos Alexandre <126966037+carlosale00@users.noreply.github.com>
2024-09-20 22:44:31 -0300

    Programa com exemplo de iteração do usuário no terminal, através de uma classe chamada scanner que auxilia nessa tarefa.

---
+++ java-anatomia-classes/src/edu/terminal/MeuPrograma.java
@@ -0,0 +1,24 @@
+package edu.terminal;
+
+/* exemplo de programa que tem iteração do usuário 
+* vamos usar uma classe para gerar essa iteraçao do usuário com o programa
+* usaremos a classe scaner 
+*/
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MeuPrograma {
+
+  public static void main(String[] args) {
+    
+    // criando objeto scaner
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    System.out.println("Digit your firstname");
+    String firstName = scanner.next();
+    System.out.println("Digit your lastname");
+    String lastName = scanner.next();
+    System.out.println("hello " + firstName + " " + lastName );
+  }
+}
+  
