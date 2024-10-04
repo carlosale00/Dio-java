19039fe1845ab5502a63667531511714dbb62f9a
Carlos Alexandre <126966037+carlosale00@users.noreply.github.com>
2024-10-03 22:07:37 -0300

    codigo pra execução da validaçao de candidatos.

---
+++ candidatura/src/Rh.java
@@ -0,0 +1,14 @@
+public class Rh extends ValidaProcessoSeletivo  {
+    
+    public static void main(String[] args) {
+
+        selecionarCandidatos();
+        // imprimeListaCandidatos();
+        // System.out.println(atender());
+        // for (String candidato : candidatos) {
+        //     entrandoEmContato(candidato);
+        // }
+
+    }
+    
+}
